299b3c9fba25177a4040fa16c6523842c4236d27
trgiahuy14 <trgiahuy14@gmail.com>
2026-03-13 12:14:02 +0700

    feat(auth): custom Breeze auth to compatible with custom jwt middleware

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,16 +4,19 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
+use App\Services\JwtService;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 use Illuminate\View\View;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Display the login view.
-     */
+    public function __construct(private JwtService $jwtService) {}
+
     public function create(): View
     {
         return view('auth.login');
@@ -24,24 +27,44 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request): RedirectResponse
     {
-        $request->authenticate();
+        $user = $request->authenticate();
 
-        $request->session()->regenerate();
+        $accessToken  = $this->jwtService->generateAccessToken($user);
+        $refreshToken = $this->jwtService->generateRefreshToken($user);
 
-        return redirect()->intended(route('dashboard', absolute: false));
+        $user->update(['refresh_token' => $refreshToken]);
+
+        return redirect()->intended(route('dashboard', absolute: false))
+            ->withCookies([
+                cookie('access_token', $accessToken, config('jwt.ttl')),
+                cookie('refresh_token', $refreshToken, config('jwt.refresh_ttl'))
+            ]);
     }
 
     /**
-     * Destroy an authenticated session.
+     * Logout user
+     * Blacklist token and remove cookies
      */
     public function destroy(Request $request): RedirectResponse
     {
-        Auth::guard('web')->logout();
+        try {
+            $user = Auth::user();
+            $accessToken = $request->cookie('access_token');
 
-        $request->session()->invalidate();
+            if ($accessToken) {
+                JWTAuth::setToken($accessToken)->invalidate();
+            }
+            if ($user) {
+                $user->update(['refresh_token' => null]);
+            }
+        } catch (\Exception $e) {
+            Log::error("Logout error: " . $e->getMessage());
+        }
 
-        $request->session()->regenerateToken();
-
-        return redirect('/');
+        return redirect('/')
+            ->withCookies([
+                cookie()->forget('access_token'),
+                cookie()->forget('refresh_token')
+            ]);
     }
 }
