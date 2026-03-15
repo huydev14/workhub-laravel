3d86c65570f357db3e2a0857bed91b3255ddcdd8
huydev14 <trgiahuy14@gmail.com>
2026-03-15 23:04:59 +0700

    feat(sidebar): dynamic active class

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,8 +3,8 @@
     <!-- Sidebar Menu -->
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-            <li class="nav-item menu-open">
-                <a href="/" class="nav-link active">
+            <li class="nav-item {{ request()->routeIs('dashboard') ? 'menu-open' : '' }}">
+                <a href="/" class="nav-link {{ request()->routeIs('dashboard') ? 'active' : '' }}">
                     <i class="nav-icon fas fa-tachometer-alt"></i>
                     <p>Dashboard</p>
                 </a>
@@ -149,7 +149,7 @@
             </li>
 
             {{-- Tài khoản --}}
-            <li class="nav-item">
+            <li class="nav-item {{ request()->routeIs('users.*') ? 'menu-is-opening menu-open' : '' }}">
                 <a href="#" class="nav-link">
                     <i class="nav-icon fas fa-users-cog"></i>
                     <p>Tài khoản
@@ -157,18 +157,19 @@
                     </p>
                 </a>
                 <ul class="nav nav-treeview">
-                    <li class="nav-item">
-                        <a href="pages/tables/simple.html" class="nav-link">
+                    <li class="nav-item ">
+                        <a href="{{ route('users.index') }}"
+                            class="nav-link {{ request()->routeIs('users.index') ? 'active' : '' }}">
                             <i class="far fa-circle nav-icon"></i>
-                            <p>Hệ thống tài khoản</p>
+                            <p>Danh sách nhân sự</p>
                         </a>
                     </li>
                 </ul>
                 <ul class="nav nav-treeview">
                     <li class="nav-item">
-                        <a href="{{ route('users.index') }}" class="nav-link">
+                        <a href="pages/tables/simple.html" class="nav-link">
                             <i class="far fa-circle nav-icon"></i>
-                            <p> Danh sách nhân sự</p>
+                            <p>Hệ thống tài khoản</p>
                         </a>
                     </li>
                 </ul>
