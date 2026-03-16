154227809dbf69fd159e08c60da03faeaa288a6b
trgiahuy14 <trgiahuy14@gmail.com>
2026-03-16 13:48:34 +0700

    feat(i18n): add laravel localization with language switcher functionality

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\JwtAuthenticate;
+use App\Http\Middleware\SetLocale;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,6 +13,12 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        // Set locale
+        $middleware->web(append: [
+            SetLocale::class
+        ]);
+
+        // JWT middleware
         $middleware->alias([
             'jwt.cookie' => JwtAuthenticate::class,
         ]);
