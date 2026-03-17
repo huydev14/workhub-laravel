c04e59ade7de674fdb0838d967a091e31a975f7d
trgiahuy14 <trgiahuy14@gmail.com>
2026-03-17 13:40:45 +0700

    feat(users): get filter options data and handle filter request

--- routes/web.php
+++ routes/web.php
@@ -31,4 +31,5 @@ require __DIR__ . '/auth.php';
 Route::middleware('jwt.cookie')->prefix('users')->name('users.')->group(function () {
     Route::get('/', [UserController::class, 'index'])->name('index');
     Route::get('/data', [UserController::class, 'data'])->name('data');
+    Route::get('/filter-data', [UserController::class, 'getFilterData'])->name('filter_data');
 });
