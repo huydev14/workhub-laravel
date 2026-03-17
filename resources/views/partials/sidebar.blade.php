dbfe3d2c3c8e9c97a562acce3cf847e5a32d1f38
huydev14 <trgiahuy14@gmail.com>
2026-03-17 23:32:51 +0700

    refactor: rearrange styling folder structure for Sass/SCSS integration

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,5 +1,5 @@
 <!-- Main Sidebar Container -->
-<aside class="sidebar tw-shadow-sm">
+<aside class="sidebar">
     <!-- Sidebar Menu -->
     <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
