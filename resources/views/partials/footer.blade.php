375ff94c7aea02ca9bd86c05f2363925e5706659
trgiahuy14 <trgiahuy14@gmail.com>
2026-03-18 17:33:40 +0700

    style(footer): decrease footer height and rename scss variables

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,8 @@
 <!-- Main Footer -->
-<footer class="footer tw-p-5 tw-text-gray-400 tw-shadow-lg">
-    &copy; {{ date('Y') }} <a href="https://github.com/huydev14/workhub-laravel"
-        class="tw-text-blue-400">{{ config('app.name') }}</a>
-    by huydev14
+<footer class="footer tw-text-gray-400 tw-shadow-lg">
     <div class="float-right d-none d-sm-inline-block">
-        <span class="tw-text-gray-400">Enterprise Resource Planning (ERP)</span>
+        <span class="tw-text-sm tw-font-semibold">&copy; {{ date('Y') }} <a
+                href="https://github.com/huydev14/workhub-laravel">{{ config('app.name') }}</a>
+            by huydev14</span>
     </div>
 </footer>
