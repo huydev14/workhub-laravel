5899db156853c2094f216f1aaab6f2772924fc04
trgiahuy14 <trgiahuy14@gmail.com>
2026-03-24 17:05:11 +0700

    style(input): update label class and adjust icon size

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -7,19 +7,19 @@
     'helper' => null,
     'type' => 'text',
     'placeholder' => '',
+    'required' => '',
 ])
 
 <div class="tw-flex tw-flex-col tw-gap-1">
-    <label for="{{ $id }}" class="tw-text-sm tw-font-semibold tw-text-gray-800">
+    <label for="{{ $id }}" class="fluent-label @if($required) is-required @endif">
         {{ $label }}
     </label>
 
-    <div
-        class="tw-relative tw-flex tw-items-center tw-bg-white tw-border tw-border-gray-300 tw-border-b-gray-400 tw-rounded-[4px] tw-overflow-hidden tw-transition-colors hover:tw-border-gray-400 focus-within:tw-border-gray-300 after:tw-content-[''] after:tw-absolute after:tw-bottom-0 after:tw-left-0 after:tw-right-0 after:tw-h-[2px] after:tw-bg-[#0063B1] after:tw-scale-x-0 focus-within:after:tw-scale-x-100 after:tw-transition-transform after:tw-duration-200 after:tw-origin-center">
+    <div class="tw-relative tw-flex tw-items-center tw-bg-white tw-border tw-border-gray-300 tw-border-b-gray-400 tw-rounded-[4px] tw-overflow-hidden tw-transition-colors hover:tw-border-gray-400 focus-within:tw-border-gray-300 after:tw-content-[''] after:tw-absolute after:tw-bottom-0 after:tw-left-0 after:tw-right-0 after:tw-h-[2px] after:tw-bg-[#0063B1] after:tw-scale-x-0 focus-within:after:tw-scale-x-100 after:tw-transition-transform after:tw-duration-200 after:tw-origin-center">
 
         @if ($icon)
             <div class="tw-pl-2.5 tw-flex tw-items-center tw-text-gray-500 tw-pointer-events-none">
-                <i class="{{ $icon }} tw-text-lg"></i>
+                <i class="{{ $icon }} tw-text-xs"></i>
             </div>
         @endif
 
