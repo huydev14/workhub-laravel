6414e212601dd1a40947e90a8187bebf8bbb3fd4
trgiahuy14 <trgiahuy14@gmail.com>
2026-03-25 23:21:01 +0700

    feat(ajax): set up CSRF token for ajax and axios requests

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,13 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+const csrfToken = document.querySelector('meta[name="csrf-token"]')?.getAttribute('content');
+
+window.axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
+
+window.$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': csrfToken,
+    },
+});
