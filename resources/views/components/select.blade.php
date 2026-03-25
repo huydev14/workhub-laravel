6918f53e92928078f07e5e6fc8a7a7b897156f0d
trgiahuy14 <trgiahuy14@gmail.com>
2026-03-25 12:20:25 +0700

    fix(select): update select component to use dynamic name attribute

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -8,7 +8,7 @@
     <label class="fluent-label" for="{{ $id }}">{{ ucfirst($title) }}</label>
     <div
         class="tw-relative tw-flex tw-items-center tw-bg-white tw-border tw-border-gray-300 tw-border-b-gray-400 tw-rounded-[4px] tw-overflow-hidden hover:tw-border-gray-400 focus-within:tw-border-gray-300 after:tw-content-[''] after:tw-absolute after:tw-bottom-0 after:tw-left-0 after:tw-right-0 after:tw-h-[2px] after:tw-bg-[#0063B1] after:tw-scale-x-0 focus-within:after:tw-scale-x-100 after:tw-transition-transform after:tw-duration-200 after:tw-origin-center">
-        <select name="department_id" id="{{ $id }}"
+        <select name="{{ $name }}" id="{{ $id }}"
             class="tw-w-full tw-py-1.5 tw-px-2.5 tw-text-sm tw-text-gray-900 tw-bg-transparent tw-border-none tw-outline-none focus:tw-ring-0 tw-appearance-none">
             <option value="">Chọn {{ strtolower($title) }}</option>
         </select>
