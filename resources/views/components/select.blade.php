c637b75f39e393513ab477ada536cd98bdfeccd0
huydev14 <trgiahuy14@gmail.com>
2026-03-26 23:39:40 +0700

    feat(components): add required attribute to select label

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -2,10 +2,14 @@
     'id' => '',
     'name' => '',
     'title' => '',
+    'required' => '',
 ])
 
 <div class="tw-flex tw-flex-col tw-gap-1">
-    <label class="fluent-label" for="{{ $id }}">{{ ucfirst($title) }}</label>
+    <label class="fluent-label @if ($required) is-required @endif"
+        for="{{ $id }}">{{ ucfirst($title) }}
+    </label>
+
     <div
         class="tw-relative tw-flex tw-items-center tw-bg-white tw-border tw-border-gray-300 tw-border-b-gray-400 tw-rounded-[4px] tw-overflow-hidden hover:tw-border-gray-400 focus-within:tw-border-gray-300 after:tw-content-[''] after:tw-absolute after:tw-bottom-0 after:tw-left-0 after:tw-right-0 after:tw-h-[2px] after:tw-bg-[#0063B1] after:tw-scale-x-0 focus-within:after:tw-scale-x-100 after:tw-transition-transform after:tw-duration-200 after:tw-origin-center">
         <select name="{{ $name }}" id="{{ $id }}"
